5b5643a3212620394a05cbe1afd65029162b63de
Thales <thales.souz@outlook.com>
2023-03-07 16:31:04 -0300

    feat: initializing req 03

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,15 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.queue import Queue
+from ting_file_management.file_management import txt_importer
+
+
+def process(path_file, instance: Queue):
+    data = txt_importer(path_file)
+
+    file = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(data),
+        "linhas_do_arquivo": data,
+    }
 
 
 def remove(instance):
