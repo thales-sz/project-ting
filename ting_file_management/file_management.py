73974909d2225a0d4c64752134c47ad5ce7fee32
Thales <thales.souz@outlook.com>
2023-03-07 16:09:52 -0300

    feat: txt_importer implemented - req 02

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,11 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implement"""
+    if not path_file.endswith('.txt'):
+        sys.stderr.write('Formato inválido\n')
+    try:
+        data = open(path_file, "r")
+        return data.read().split('\n')
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
