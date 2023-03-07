de207d4b92fb1ae4d3bdaa1028bce683d9014dba
Thales <thales.souz@outlook.com>
2023-03-07 15:39:37 -0300

    feat: queue FIFO, length, enqueue, dequeue works

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,16 +3,16 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self._queue = []
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self._queue)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self._queue.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        self._queue.pop()
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        
