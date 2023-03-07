047fb65b0b86dc378b6d514ac9676d82f3f7a91b
Thales <thales.souz@outlook.com>
2023-03-07 16:49:14 -0300

    feat: verify if the instance has the path_file correct

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,15 +1,19 @@
+import sys
 from ting_file_management.queue import Queue
 from ting_file_management.file_management import txt_importer
 
 
 def process(path_file, instance: Queue):
-    data = txt_importer(path_file)
-
-    file = {
-        "nome_do_arquivo": path_file,
-        "qtd_linhas": len(data),
-        "linhas_do_arquivo": data,
-    }
+    for i in range(len(instance)):
+        if instance.search(i)["nome_do_arquivo"] == path_file:
+            data = txt_importer(path_file)
+            file = {
+                "nome_do_arquivo": path_file,
+                "qtd_linhas": len(data),
+                "linhas_do_arquivo": data,
+            }
+            instance.enqueue(file)
+            sys.stdout.write(str(file))
 
 
 def remove(instance):
