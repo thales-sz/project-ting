75640232beeb7b9761ca04a83e75ef13b7293a73
Thales <thales.souz@outlook.com>
2023-03-08 15:13:16 -0300

    test: test for IndexError and queue length

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -2,7 +2,17 @@ from ting_file_management.priority_queue import PriorityQueue
 import pytest
 
 
+MOCK_1 = {"qtd_linhas": 7}
+MOCK_2 = {"qtd_linhas": 2}
+
+
 def test_basic_priority_queueing():
     queue = PriorityQueue()
 
-    queue.enqueue()
+    queue.enqueue(MOCK_1)
+    queue.enqueue(MOCK_2)
+
+    with pytest.raises(IndexError):
+        queue.search(89)
+    
+    assert queue.__len__() == 2
