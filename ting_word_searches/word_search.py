204962c538b809b66a0ee47f3f4f3a3bf6a3055e
Thales <thales.souz@outlook.com>
2023-03-08 15:40:17 -0300

    feat: exists_word implemented - req 07

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,24 @@
-def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.queue import Queue
 
 
-def search_by_word(word, instance):
+def exists_word(word, instance: Queue):
+    result = list()
+    for index in range(len(instance)):
+        file = instance.search(index)
+        data = {
+            "palavra": word,
+            "arquivo": file["nome_do_arquivo"],
+            "ocorrencias": [],
+        }
+
+        for line in range(file["qtd_linhas"]):
+            if word.lower() in file["linhas_do_arquivo"][line].lower():
+                data["ocorrencias"].append({"linha": line + 1})
+
+        if len(data["ocorrencias"]):
+            result.append(data)
+    return result
+
+
+def search_by_word(word, instance: Queue):
     """Aqui irá sua implementação"""
