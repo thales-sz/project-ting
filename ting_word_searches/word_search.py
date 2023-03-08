6864a1d001a2b8937ee643041839f5ca0135dab5
Thales <thales.souz@outlook.com>
2023-03-08 15:54:05 -0300

    feat: search_by_word implemented - req 08

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -21,4 +21,22 @@ def exists_word(word, instance: Queue):
 
 
 def search_by_word(word, instance: Queue):
-    """Aqui irá sua implementação"""
+    result = list()
+    for index in range(len(instance)):
+        file = instance.search(index)
+        data = {
+            "palavra": word,
+            "arquivo": file["nome_do_arquivo"],
+            "ocorrencias": [],
+        }
+
+        for line in range(file["qtd_linhas"]):
+            if word.lower() in file["linhas_do_arquivo"][line].lower():
+                data["ocorrencias"].append({
+                    "linha": line + 1,
+                    "conteudo": file["linhas_do_arquivo"][line],
+                    })
+
+        if len(data["ocorrencias"]):
+            result.append(data)
+    return result
